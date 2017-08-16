234d7c69749ecd5fdb88592169fa5e17cd029815
Chenhao <chenh445@gmail.com>
2017-08-16 17:06:45 -0400

    fix score display issue

--- js/game.js
+++ js/game.js
@@ -32,41 +32,6 @@ $(document).keydown(function(event) {
   
 });
 
-/**
-* decide if the cell can move left or not,
-* if can, complete move left operation
-*/ 
-/*function moveLeft() {
-  if (!canMoveLeft(board)) { // cannot move left
-    return false;
-  } else { 
-    // logic for move left operation
-    for (var i = 0; i < 4; i++) {
-      for (var j = 1; j < 4; j++) {
-        // the current cell must have value
-        if (board[i][j] != 0) {
-          // move the cell to left
-          for (var k = 0; k < j; k++) {
-            if (board[i][k] == 0 && noBlockHorizontalCol(i, k, j, board)) {
-              // current cell has value and all the left cells are 0, can move left
-              showMoveAnimation(i, j, i, k);
-              board[i][k] = board[i][j];
-              board[i][j] = 0;
-            } else if (board[i][k] == board[i][j] && noBlockHorizontalCol(i, k, j, board)) {
-              // current cell and left cell values are equal, and cells in between are 0, can move left
-              showMoveAnimation(i, j, i, k);
-              board[i][k] += board[i][j] // add the values
-              board[i][j] = 0;
-            }
-          }
-        }
-      }
-    }
-    setTimeout("updateBoardView();", 200);
-    return true;
-  }
-}*/
-
 
 function IsGameOver(){
   if(!canMoveLeft(board) && !canMoveRight(board) && !canMoveUp(board) && !canMoveDown(board)){
@@ -116,38 +81,6 @@ function moveLeft() {
   }
 }
 
-/*
-function moveRight() {
-  if (!canMoveRight(board)) { // cannot move right
-    return false;
-  } else { 
-    // logic for move right operation
-    for (var i = 0; i < 4; i++) {
-      for (var j = 0; j < 3; j++) {
-        // the current cell must have value
-        if (board[i][j] != 0) {
-          // move the cell to right
-          for (var k = 3; k > j; k--) {
-            if (board[i][k] == 0 && noBlockHorizontalCol(i, k, j, board)) {
-              // current cell has value and all the right cells are 0, can move right
-              showMoveAnimation(i, j, i, k);
-              board[i][k] = board[i][j];
-              board[i][j] = 0;
-            } else if (board[i][k] == board[i][j] && noBlockHorizontalCol(i, k, j, board)) {
-              // current cell and right cell values are equal, and cells in between are 0, can move right
-              showMoveAnimation(i, j, i, k);
-              board[i][k] += board[i][j] // add the values
-              board[i][j] = 0;
-            }
-          }
-        }
-      }
-    }
-    setTimeout("updateBoardView();", 200);
-    return true;
-  }
-}*/
-
 function moveRight() {
   if (!canMoveRight(board)) { // cannot move left
     return false;
@@ -190,42 +123,6 @@ function moveRight() {
   }
 }
 
-/*
-function moveUp() {
-  if (!canMoveUp(board)) { // cannot move up
-    return false;
-  } else { 
-    // logic for move up operation
-    for (var i = 0; i < 4; i++) {
-      for (var j = 1; j < 4; j++) {
-        // the current cell must have value
-        if (board[j][i] != 0) {
-          // move the cell to top
-          for (var k = 0; k < j; k++) {
-            if (board[k][i] == 0 && noBlockVerticalRow(j, k, j, board)) {
-              // current cell has value and all the top cells are 0, can move up
-              showMoveAnimation(j, i, k, i);
-              board[k][i] = board[j][i];
-              board[j][i] = 0;
-            } else if (board[k][i] == board[j][i] && noBlockVerticalRow(k, i, j, board)) {
-              // current cell and top cell values are equal, and cells in between are 0, can move up
-              showMoveAnimation(j, i, k, i);
-              board[k][i] += board[j][i] // add the values
-              board[j][i] = 0;
-            }
-          }
-        }
-      }
-    }
-    setTimeout("updateBoardView();", 200);
-    return true;
-  }
-}*/
-
-
-
-
-
 function moveUp() {
   if (!canMoveUp(board)) { // cannot move left
     return false;
@@ -267,38 +164,6 @@ function moveUp() {
     return true;
   }
 }
-
-
-/*function moveDown() {
-  if (!canMoveDown(board)) { // cannot move down
-    return false;
-  } else { 
-    // logic for move up operation
-    for (var i = 0; i < 4; i++) {
-      for (var j = 0; j < 3; j++) {
-        // the current cell must have value
-        if (board[j][i] != 0) {
-          // move the cell to bottom
-          for (var k = 3; k > j; k--) {
-            if (board[k][i] == 0 && noBlockVerticalRow(j, k, j, board)) {
-              // current cell has value and all the bottom cells are 0, can move down
-              showMoveAnimation(j, i, k, i);
-              board[k][i] = board[j][i];
-              board[j][i] = 0;
-            } else if (board[k][i] == board[j][i] && noBlockVerticalRow(k, i, j, board)) {
-              // current cell and bottom cell values are equal, and cells in between are 0, can move down
-              showMoveAnimation(j, i, k, i);
-              board[k][i] += board[j][i] // add the values
-              board[j][i] = 0;
-            }
-          }
-        }
-      }
-    }
-    setTimeout("updateBoardView();", 200);
-    return true;
-  }
-}*/
 
 function moveDown() {
   if (!canMoveDown(board)) { // cannot move left
